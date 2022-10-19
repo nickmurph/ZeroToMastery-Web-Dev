57973a9c007af1a854673130ec43c73d7504390e
nickmurph <nfmurphy@buffalo.edu>
2022-10-19 02:09:02 -0600

    further JS intro exercises

--- section11 js exercises/JS and HTML demo/script.js
+++ section11 js exercises/JS and HTML demo/script.js
@@ -1,6 +1,37 @@
-4 + 3;
 
-if (4+3 === 7) {
-	// alert("External JS file executed in HTML!");
-	console.log("External JS file printing to the console");
-}
+// demonstrating simplistic interaction between HTML and JS
+//
+// 4 + 3;
+// if (4+3 === 7) {
+// 	alert("External JS file executed in HTML!");
+// 	console.log("External JS file printing to the console");
+// }
+
+
+// demonstrating function declaration and function expression
+//
+// function sayHello(){
+// 	console.log("Hello");
+// }
+// sayHello();
+
+// var sayBye = function () {
+// 	console.log("Bye");
+// }
+// sayBye();
+
+
+//Demonstrating parameterized functions
+//
+// function sing(songLine){
+// 	console.log(songLine);
+// }
+// sing("EEEEHHH");
+// sing("LA DI DA");
+// sing("I'm a Dweller on the Threshold");
+
+
+// function multiply (a,b){
+// 	return a*b;
+// }
+// console.log(multiply(2,3));
