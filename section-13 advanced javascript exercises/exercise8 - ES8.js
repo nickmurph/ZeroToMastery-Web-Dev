62e472d5b44a2ce48a4c45619f9331b4be7614fd
nickmurph <nfmurphy@buffalo.edu>
2022-11-17 16:49:42 -0700

    Update exercise8 - ES8.js

--- section-13 advanced javascript exercises/exercise8 - ES8.js
+++ section-13 advanced javascript exercises/exercise8 - ES8.js
@@ -16,16 +16,44 @@ console.log(turtle);
 console.log(rabbit);
 
 
+
+//ANS
+turtle = turtle.padStart(8);
+rabbit = rabbit.padStart(8);
+console.log(startLine);
+console.log(turtle);
+console.log(rabbit);
+
+
+
 // #2) What happens when you run turtle.trim().padEnd(9, '=') on the turtle variable
 // Read about what the second parameter does in padEnd and padStart
 turtle = turtle.trim().padEnd(9, '=');
+
+//ANS
+// Trim method removes any whitespace from either end of the string and returns it as a new string
+// Trim followed by Pad will trim the whitespace and then pad the string as directed
+// Padding with a second argument will use the string from that argument instead of whitespace to pad
+// Note: trimStart and trimEnd methods exist to trim whitespace from either side
+
 
 
 // #3) Get the below object to go from:
 let obj = {
   my: 'name',
   is: 'Rudolf',
-  the: 'raindeer'
+  the: 'reindeer'
 }
 // to this:
-'my name is Rudolf the raindeer'
+'my name is Rudolf the reindeer'
+
+
+//ANS
+ansString = ""
+
+Object.entries(obj).forEach( ([key,value]) => {
+  ansString = ansString + key + ' '
+  ansString = ansString + value + ' '
+})
+ansString = ansString.trimEnd()
+console.log(ansString)
