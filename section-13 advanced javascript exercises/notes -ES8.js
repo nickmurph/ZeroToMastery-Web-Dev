441b7ba56a9ae8e2153796cdfe716af036955de6
nickmurph <nfmurphy@buffalo.edu>
2022-11-17 16:03:41 -0700

    Update notes -ES8.js

--- section-13 advanced javascript exercises/notes -ES8.js
+++ section-13 advanced javascript exercises/notes -ES8.js
@@ -2,9 +2,9 @@
 'Turtle'.padStart(10); //evaluates to "    Turtle"
 'Turtle'.padEnd(10); //evaluates to "Turtle    "
 'Turtle'.padStart(7, '_'); //evaluates to "_Turtle"
-//Note that the number given as a padding parameter includes the length of the string being padded
+//Note that the number given as a padding argument includes the length of the string being padded
 'Turtle'.padStart(6, '_'); //evaluates to "Turtle" because Turtle is len 6
-//Thus, any padding parameter of equal or less length to the string will have no effect
+//Thus, any padding argument of equal or less length to the string will have no effect
 //This is useful for aligning multiple strings
 console.log("Turtle".padStart(10))
 console.log("Burt".padStart(10))
@@ -17,3 +17,11 @@ console.log("Athenaeum".padStart(10))
 
 
 //
+
+// note the comma after the fourth parameter, d
+const fun = (a,b,c,d,) => {
+	console.log(a);
+}
+
+// note the comma after the fourth argument, 4
+fun 
