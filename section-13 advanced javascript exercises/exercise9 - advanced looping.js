40dbb03ea2e0099379a2c58dcf46ef9f7c7766de
nickmurph <nfmurphy@buffalo.edu>
2022-11-17 20:07:56 -0700

    Create exercise9 - advanced looping.js

---
+++ section-13 advanced javascript exercises/exercise9 - advanced looping.js
@@ -0,0 +1,95 @@
+const basket = ['apples', 'oranges', 'grapes'];
+const detailedBasket = {
+  apples: 5,
+  oranges: 10,
+  grapes: 1000
+}
+
+//1
+for (let i = 0; i < basket.length; i++) {
+  console.log(basket[i]);
+}
+
+//2
+basket.forEach(item => {
+  console.log(item);
+})
+
+for (item in detailedBasket) {
+  console.log(item);
+}
+
+for (item of basket) {
+  console.log(item);
+}
+
+// Question #1:
+// create a function called biggestNumberInArray() that takes
+// an array as a parameter and returns the biggest number.
+// biggestNumberInArray([-1,0,3,100, 99, 2, 99]) should return 100;
+// Use at least 3 different types of javascript loops to write this:
+const array = [-1,0,3,100, 99, 2, 99] // should return 100
+const array2 = ['a', 3, 4, 2] // should return 4
+const array3 = [] // should return 0
+
+//ANS
+function biggestNumberInArray(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  curBiggest = -Infinity
+  for (let i=0; i < arr.length; i++){
+    if (arr[i] > curBiggest) {
+      curBiggest = arr[i];
+    }
+  }
+  return curBiggest;
+}
+
+
+
+function biggestNumberInArray2(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  curBiggest = -Infinity
+  for (num of arr){
+    if (num > curBiggest){
+      curBiggest = num;
+    }
+  }
+  return curBiggest;
+}
+
+function biggestNumberInArray3(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  curBiggest = -Infinity
+  for (index in arr){
+    if (arr[index] > curBiggest) {
+      curBiggest = arr[index];
+    }
+  }
+  return curBiggest;
+}
+  
+
+
+// Question #2:
+// Write a function checkBasket() that lets you know if the item is in the basket or not
+amazonBasket = {
+  glasses: 1,
+  books: 2,
+  floss: 100
+}
+
+//ANS
+function checkBasket(basket, lookingFor) {
+  for (item in basket){
+    if (item === lookingFor){
+      return true
+    }
+  }
+  return false
+}
