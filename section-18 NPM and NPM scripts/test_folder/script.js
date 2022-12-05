9fc8de5aebdb1dd8aff56ba2536b9550947cc50e
nickmurph <nfmurphy@buffalo.edu>
2022-12-05 12:07:35 -0700

    package demo-ing

---
+++ section-18 NPM and NPM scripts/test_folder/script.js
@@ -0,0 +1,10 @@
+import { without } from '/node_modules/lodash/without.js';
+// var _ = require('lodash');
+
+
+
+
+var array = [1,2,3,4,5,6,7,8];
+console.log('answer: ', without(array, 3));
+// console.log('answer: ', _.without(array, 3));
+
