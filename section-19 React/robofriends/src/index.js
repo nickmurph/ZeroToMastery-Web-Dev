4b57de7e28671c3f11bf930b4c300e1e03e008ad
nickmurph <nfmurphy@buffalo.edu>
2022-12-19 17:15:27 -0700

    sec 233 - Building React App part 3, begun

--- section-19 React/robofriends/src/index.js
+++ section-19 React/robofriends/src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+
+import App from './App'
 import { robots } from './Robots'
 import CardList from './CardList'
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardList robots={robots}/>
+    <App/>
   </React.StrictMode>
 );
 
