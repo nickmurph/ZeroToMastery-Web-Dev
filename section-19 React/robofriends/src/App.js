a676e38efe5064cc89e4bc0aac9f05d291c746cd
nickmurph <nfmurphy@buffalo.edu>
2022-12-19 19:57:13 -0700

    sec 236 - using an API for user data instead of hardcoding, loading screen if APi is slow or fails

--- section-19 React/robofriends/src/App.js
+++ section-19 React/robofriends/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import CardList from './CardList'
-import {robots} from './Robots'
 import SearchBox from './SearchBox'
 import './App.css'
 
@@ -9,9 +8,16 @@ class App extends React.Component {
     constructor(){
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: '',
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}) //replace {robots: users} with {} to test the empty robots loading page
+        )
     }
 
     onSearchChange = (event) => {
@@ -22,7 +28,9 @@ class App extends React.Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         })
-
+        if (this.state.robots.length === 0){
+            return <h1 className='tc f1'>Loading</h1>
+        }else {
         return (
             <div className='tc'>
             <h1 className='f1'>RoboFriends</h1>
@@ -30,6 +38,7 @@ class App extends React.Component {
             <CardList robots={filteredRobots}/>
             </div>
         );
+        }
     }
 }
 
