7f4c72d291120888f45c7dc06ee1653774961572
nickmurph <nfmurphy@buffalo.edu>
2022-12-19 18:07:32 -0700

    sec 233 continued

--- section-19 React/robofriends/src/App.js
+++ section-19 React/robofriends/src/App.js
@@ -4,15 +4,29 @@ import {robots} from './Robots'
 import SearchBox from './SearchBox'
 
 
-const App = () => {
-    return (
-        <div className='tc'>
-        <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={robots}/>
-        </div>
-    );
-}
 
+class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            robots: robots,
+            searchField: '',
+        }
+    }
+
+    onSearchChange(event){
+        console.log(event.target.value)
+    }
+
+    render(){
+        return (
+            <div className='tc'>
+            <h1>RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange} />
+            <CardList robots={this.state.robots}/>
+            </div>
+        );
+    }
+}
 
 export default App;
